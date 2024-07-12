e1b0a6674509386d61f9b284f1f40f14781c1954
stephenamills <stephmills468@gmail.com>
2024-07-12 16:57:31 -0700

    Change .prettierrc back to JSON format

--- typescript/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-	printWidth: 100,
-	semi: false,
-	plugins: ['prettier-plugin-tailwindcss'], // Tailwind CSS must come last https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
-	tailwindConfig: "./tailwind.config.ts"
-}
